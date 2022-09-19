069359b292fde3b19ddf12678077448edf09ba49
adityajalu <101347792+adityajalu@users.noreply.github.com>
2022-09-19 16:25:40 +0700

    menambahkan file

---
+++ src/main/java/mii/co/id/warehouseserverside/WarehouseServersideApplication.java
@@ -0,0 +1,13 @@
+package mii.co.id.warehouseserverside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseServersideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehouseServersideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mii/co/id/warehouseserverside/WarehouseServersideApplicationTests.java
@@ -0,0 +1,13 @@
+package mii.co.id.warehouseserverside;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseServersideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
